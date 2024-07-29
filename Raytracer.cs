ad8523ec60bf66100da23e845924dcc6ce37f70c
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-07-29 11:32:27 +0200

    Render gradient in circle The circle gets resized with the screen

--- Raytracer.cs
+++ Raytracer.cs
@@ -17,10 +17,42 @@ namespace raytracer
             {
                 for (int y = 0; y < surface.height; y++)
                 {
-                    surface.SetPixel(x, y, 0.6f, 1.0f, 0.2f);
+                    Color color = new Color(0f, 0f, 0f);
+
+                    float gradientX = x / (float)surface.width;
+                    float gradientY = y / (float)surface.height;
+
+                    int centerX = surface.width / 2;
+                    int centerY = surface.height / 2;
+
+                    int distX = System.Math.Abs(x - centerX);
+                    int distY = System.Math.Abs(y - centerY);
+                    float distFromCenter = System.MathF.Sqrt(System.MathF.Pow(distX, 2) + System.MathF.Pow(distY, 2)); // pythagoras
+
+                    float avgScreenSize = (surface.width + surface.height) / 2f;
+                    float circleSize = avgScreenSize / 5f;
+
+                    if (distFromCenter < circleSize) {
+                        color.r = gradientX;
+                        color.g = gradientY;
+                    }
+
+                    surface.SetPixel(x, y, color.r, color.g, color.b);
                 }
             }
         }
 
+    }
+
+    struct Color {
+        public float r { get; set; }
+        public float g { get; set; }
+        public float b { get; set; }
+        public Color(float r, float g, float b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
     }
 }
--- obj/Debug/net8.0/raytracer.AssemblyInfo.cs
+++ obj/Debug/net8.0/raytracer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("raytracer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f547f2330d4875f96e5e622f88a9c0c951b12f")]
 [assembly: System.Reflection.AssemblyProductAttribute("raytracer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("raytracer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
